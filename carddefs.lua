f8e556bfec721bfcd7f4e415b9a3c5f3979bd829
Evan Paul Lara <evplara@ucsc.edu>
2025-06-12 18:40:59 -0700

    Final
    Undo, Visualization Changes, Card Inspect & Description, Title Screen

--- carddefs.lua
+++ carddefs.lua
@@ -4,27 +4,32 @@ local CardDefs = {
     WoodenCow = {
         cost = 1,
         basePower = 1,
+        description = "A humble wooden cow.",
         onReveal = function(self, game) end
     },
     Pegasus = {
         cost = 3,
         basePower = 5,
+        description = "A majestic flying horse.",
         onReveal = function(self, game) end
     },
     Minotaur = {
         cost = 5,
         basePower = 9,
+        description = "A fearsome beast of legend.",
         onReveal = function(self, game) end
     },
     Titan = {
         cost = 6,
         basePower = 12,
+        description = "A mighty titan.",
         onReveal = function(self, game) end
     },
 
     Zeus = {
         cost = 5,
         basePower = 4,
+        description = "Reduces power of all cards here by 1.",
         onReveal = function(self, game)
             local loc = self.location
             for _, c in ipairs(game.zones[self.owner][loc]) do
@@ -39,6 +44,7 @@ local CardDefs = {
     Artemis = {
         cost = 4,
         basePower = 3,
+        description = "+5 power if exactly one enemy card here.",
         onReveal = function(self, game)
             local loc = self.location
             local enemyCount = #game.zones[self.opponent][loc]
@@ -51,6 +57,7 @@ local CardDefs = {
     Athena = {
         cost = 2,
         basePower = 2,
+        description = "Gains +1 power when you play a card here this turn.",
         onReveal = function(self, game)
             local loc = self.location
             table.insert(game.listeners, function(ev)
@@ -68,6 +75,7 @@ local CardDefs = {
     Apollo = {
         cost = 2,
         basePower = 1,
+        description = "You gain +1 mana next turn.",
         onReveal = function(self, game)
             game.nextTurnManaBonus[self.owner] =
                 (game.nextTurnManaBonus[self.owner] or 0) + 1
@@ -77,6 +85,7 @@ local CardDefs = {
     Demeter = {
         cost = 3,
         basePower = 0,
+        description = "Each player draws a card.",
         onReveal = function(self, game)
             game:drawCard(self.owner)
             game:drawCard(self.opponent)
@@ -86,6 +95,7 @@ local CardDefs = {
     Midas = {
         cost = 5,
         basePower = 5,
+        description = "All cards here become 3 power.",
         onReveal = function(self, game)
             local loc = self.location
             for _, c in ipairs(game.zones[self.owner][loc]) do
@@ -100,6 +110,7 @@ local CardDefs = {
     Ares = {
         cost = 5,
         basePower = 6,
+        description = "+2 power for each enemy card here.",
         onReveal = function(self, game)
             local loc = self.location
             local numEnemy = #game.zones[self.opponent][loc]
@@ -110,6 +121,7 @@ local CardDefs = {
     Hades = {
         cost = 4,
         basePower = 3,
+        description = "+2 power for each card in your discard.",
         onReveal = function(self, game)
             local discardCount = #game.discards[self.owner]
             self.currentPower = self.currentPower + 2 * discardCount
@@ -119,6 +131,7 @@ local CardDefs = {
     Hera = {
         cost = 3,
         basePower = 2,
+        description = "Cards in your hand get +1 power.",
         onReveal = function(self, game)
             for _, c in ipairs(game.hands[self.owner]) do
                 c.currentPower = c.currentPower + 1
@@ -129,6 +142,7 @@ local CardDefs = {
     Prometheus = {
         cost = 4,
         basePower = 2,
+        description = "Opponent draws a card.",
         onReveal = function(self, game)
             game:drawCard(self.opponent)
         end
